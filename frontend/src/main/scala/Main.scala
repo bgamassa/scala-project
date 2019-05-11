a30f54868aa227bea111c5f3a357dc7db9124b00
Binta Gamassa <bintagam@yahoo.fr>
2019-05-11 18:43:26 +0200

    [FRONTEND] data display changed

--- frontend/src/main/scala/Main.scala
+++ frontend/src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.scalajs.dom
 import dom.document
+import dom.html
 import dom.ext.Ajax
 import scala.io.Source
 import java.net.URL
@@ -9,31 +10,57 @@ import scala.concurrent.duration._
 import scala.io._
 import scala.scalajs.js.timers._
 import scala.util._
+import scala.scalajs.js.{JSON, URIUtils}
+import scala.scalajs.js
+
 
 object Main extends App {
+
+  case class Report(
+    date: String,
+    gps_fix: Int,
+    latitude: Float,
+    longitude: Float,
+    altitude: Float,
+    temperature: Float,
+    battery: Float,
+    extra: String
+  )
+
     def appendPar(targetNode: dom.Node, text: String): Unit = {
+    
     val parNode = document.createElement("p")
     val textNode = document.createTextNode(text)
     parNode.appendChild(textNode)
     targetNode.appendChild(parNode)
   }
 
-  def getData() : Unit = {
-      val url =
-        "http://localhost:9000/data"
-        Ajax.get(
-          url = url,
-          headers = Map(
-            "Content-type" -> "application/json",
-            "Access-Control-Allow-Origin" -> "http://localhost:5000",
-          )
-          ).onSuccess { case xhr =>
-          appendPar(document.body, xhr.responseText)
-        }
+  def JSONParse(targetNode: dom.Node, text: String) {
+    JSON.parse(text).asInstanceOf[js.Array[js.Dynamic]].foreach{
+      elt => appendPar(targetNode, "id : %s  - Date: %s - Gps_fix: %s - Latitude: %s - Longitude: %s ".format(
+        elt.id, elt.date, elt.gps_fix, elt.latitude, elt.longitude)
+      + "Altitude: %s - Temperature: %s - Battery: %s - Extra: %s".format(elt.altitude, elt.temperature, elt.battery, elt.extra))
+    }
 
-    setTimeout(7000) { getData() }
+    val max = JSON.parse(text).asInstanceOf[js.Array[js.Dynamic]].reduceLeft ( _ max _ ).max
+    println(max)
+
+    setTimeout(10000) { getData(targetNode, max.toString()) }
   }
 
-  //appendPar(document.body, getData())
-  getData()
+  def getData(targetNode: dom.Node, id: String) : Unit = {
+      val url = "http://scala-aggregator-api.eu-gb.mybluemix.net/data/all?minID=" + id
+      Ajax.get(url).onSuccess { case xhr =>
+       JSONParse(div, xhr.responseText)
+      }
+  }
+
+  val div = document.createElement("div")
+  val h1Node = document.createElement("h1")
+  val h1value = document.createTextNode("Flux de données")
+
+  h1Node.appendChild(h1value)
+  div.appendChild(h1Node)
+  document.body.appendChild(div)
+  getData(div, "1")
 }
